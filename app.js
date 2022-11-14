e62044e8d6c7abf69780eedb7f60215d85c43169
manzeelanepali <nepalimanzeela@gmail.com>
2022-11-14 14:16:32 +0545

    made utils folder and config.js file inside of it

---
+++ app.js
@@ -0,0 +1,38 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb+srv://ManjilaNep:CWfDWJwGtqp8tLXV@cluster0.6bqbifp.mongodb.net/bloglist?retryWrites=true&w=majority'
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
