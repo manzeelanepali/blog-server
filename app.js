173a65e5378dcf2bb5ca3ab5891ce0fbf93314b1
manzeelanepali <nepalimanzeela@gmail.com>
2022-11-14 16:27:54 +0545

    codesBeforeAcessingtheCommonUrlRoutesfromControler

--- app.js
+++ app.js
@@ -1,42 +1,40 @@
-const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
 // const notesRouter = require('./controllers/notes')
 // const middleware = require('./utils/middleware')
 // const logger = require('./utils/logger')
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
-})
+});
 
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = mongoose.model("Blog", blogSchema);
 
-const mongoUrl = 'mongodb+srv://ManjilaNep:CWfDWJwGtqp8tLXV@cluster0.6bqbifp.mongodb.net/bloglist?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl)
+const mongoUrl =
+  "mongodb+srv://ManjilaNep:CWfDWJwGtqp8tLXV@cluster0.6bqbifp.mongodb.net/bloglist?retryWrites=true&w=majority";
+mongoose.connect(mongoUrl);
 // console.log(mongoUrl)
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/api/blogs', (request, response) => {
-    Blog.find({}).then(blogs => {
-      response.json(blogs)
-    })
-  })
-  
+app.get("/api/blogs", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+app.post("/api/blogs", (request, response) => {
+  const blog = new Blog(request.body);
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
 
-module.exports = app
+module.exports = app;
