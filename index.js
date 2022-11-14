a6311d834ee21a70bd37786c4f68c0dd0afac8da
manzeelanepali <nepalimanzeela@gmail.com>
2022-11-14 11:49:17 +0545

    copied the index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
 
 const Blog = mongoose.model('Blog', blogSchema)
 
