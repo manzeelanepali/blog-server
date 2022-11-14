b14351ee8b96152324cc1ab64745e0dd0669cc18
manzeelanepali <nepalimanzeela@gmail.com>
2022-11-14 15:52:10 +0545

    refactoring all the codes

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const http = require('http')
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+// const notesRouter = require('./controllers/notes')
+// const middleware = require('./utils/middleware')
+// const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
 const blogSchema = new mongoose.Schema({
@@ -14,17 +17,17 @@ const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = 'mongodb+srv://ManjilaNep:CWfDWJwGtqp8tLXV@cluster0.6bqbifp.mongodb.net/bloglist?retryWrites=true&w=majority'
 mongoose.connect(mongoUrl)
+// console.log(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
 
 app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
+    Blog.find({}).then(blogs => {
       response.json(blogs)
     })
-})
+  })
+  
 
 app.post('/api/blogs', (request, response) => {
   const blog = new Blog(request.body)
@@ -36,3 +39,4 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
+module.exports = app
