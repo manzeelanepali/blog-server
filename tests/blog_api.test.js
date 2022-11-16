7e47b97982903bb97f3ae48aa4fd42cb21219ea8
manzeelanepali <nepalimanzeela@gmail.com>
2022-11-16 11:46:06 +0545

    made another test case blog_api.test and made the test to get passed

---
+++ tests/blog_api.test.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+
+const api = supertest(app); // supertest le app lai wrap garyo
+
+test("blogs are returned as json", async () => {
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    //.expect("Content-Type", "application/json; charset=utf-8");
+    .expect("Content-Type", /application\/json/); // this is regular expression
+  // this is regular expression
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
