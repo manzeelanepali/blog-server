1535c3ce1fcdfea3e33fbb38df4f1f999a92c258
manzeelanepali <nepalimanzeela@gmail.com>
2022-11-23 14:47:40 +0545

    feat : generation of the token

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const Blog = require("./models/blogs");
 const { response } = require("express");
 const blogRouter = require("./controllers/blog");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 // const middleware = require('./utils/middleware')
 // const logger = require('./utils/logger')
